2b7aa18ab555c9964b969e0e4903aaa326162c73
alex <alex-just2200@yandex.ru>
2019-06-03 11:29:28 +0300

    fix

--- quick_sort.py
+++ quick_sort.py
@@ -1,9 +1,8 @@
-#from random import randint
-#from decorators import find_time_for_execute as ft
+from random import randint
+from decorators import find_time_for_execute as ft
 
-#numbers = [randint(1,11) for i in range(100000)]
 
-#@ft
+@ft
 def quick_sort(array):
     #быстрая сортировка
     if array == []:
@@ -16,5 +15,8 @@ def quick_sort(array):
         greater =  [i for i in array if i > choice]
         return quick_sort(less)  + mid + quick_sort(greater)
 
-#print(numbers)
-#print(quick_sort(numbers))
+if __name__ == '__main__':
+
+    numbers = [randint(1,11) for i in range(100000)]
+    print(numbers)
+    print(quick_sort(numbers))
